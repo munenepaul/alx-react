e68df4ecbefc1832c6eb8bce5fe7299949932509
munenepaul <evansmunenepaul@gmail.com>
2024-01-04 17:40:51 +0000

    done

---
+++ 0x01-react_intro/task_1/dashboard/src/utils.js
@@ -0,0 +1,12 @@
+export function getFullYear() {
+  let today = new Date();
+  return today.getFullYear();
+}
+
+export function getFooterCopy(isIndex) {
+  if (isIndex === true) {
+    return "Holberton School";
+  } else {
+    return "Holberton School main dashboard";
+  }
+}
